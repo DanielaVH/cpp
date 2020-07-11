a70f9558479a0c400dcb5b4b3046c16ff19264bb
Daniela Velasquez <sarahihernandezvelasquez14@gmail.com>
2020-07-11 16:39:22 -0600

    Agregando Ejercicio 8

--- Ejercicios/Ejercicio7-Factura/Factura.cpp
+++ Ejercicios/Ejercicio7-Factura/Factura.cpp
@@ -2,23 +2,24 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+int  main ( int argc, char  const * argv [])
 {
-    //Datos de entrada
-    double subtotal= 0;
-    double total=0;
-    double impuesto= 0.15;
+    // Datos de entrada
+    double  subtotal = 0 ;
+    double total = 0 ;
+    double impuesto = 0.15 ;
 
-    cout << "Ingrese el subtotal de la factura :";
+    cout << " Ingrese el subtotal de la factura: " ;
     cin >> subtotal;
 
-//Proceso
-total= subtotal + (subtotal * 0.15);
+// Proceso
+total = subtotal + (subtotal * 0.15 );
 
 
-//Salida
-cout<< endl;
-cout<<"Total a pagar es:" << total;
+// Salida
+cout << endl;
+cout << " Total a pagar es: " << total;
 
-    return 0;
-}
+  return 0;
+  }
+  
