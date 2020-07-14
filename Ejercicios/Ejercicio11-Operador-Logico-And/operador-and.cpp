b66026fd359855968a84945f5f8022b0206a15c6
Daniela Velasquez <sarahihernandezvelasquez14@gmail.com>
2020-07-13 20:46:09 -0600

    Agregando Ejercicio 11

--- Ejercicios/Ejercicio11-Operador-Logico-And/operador-and.cpp
+++ Ejercicios/Ejercicio11-Operador-Logico-And/operador-and.cpp
@@ -8,5 +8,30 @@ int main(int argc, char const *argv[])
     int b= 10;
     int c=7;
 
+// if (a>b && a>c) {
+// cout << "a es el mayor:" << a;
+// } else if(b> a && b>c){
+//     cout << "b es el mayor:" << b;
+// }else {
+//     cout << "c es el mayor";
+// }
+//     return 0;
+// }
+
+
+if (a>b && a>c) 
+{
+cout << "a es el mayor:" << a;
+} 
+if(b> a && b>c)
+{
+    cout << "b es el mayor:" << b;
+}
+if(c>a && c>b)
+{
+
+cout << "c es el mayor:" << c;
+}
     return 0;
 }
+
