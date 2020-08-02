dc2780e5643dba4a9dee251e2550b8a4c14c92b2
Daniela Velasquez <sarahihernandezvelasquez14@gmail.com>
2020-08-02 14:58:19 -0600

    Ejercicio44

--- Ejercicios/Ejercicio44-Punto-de-Venta-parte-V/factura.cpp
+++ Ejercicios/Ejercicio44-Punto-de-Venta-parte-V/factura.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 double subtotal;
+double total;
+double impuesto = 0.15;
+
 string listaProductos;
 
 void agregarProducto(string descripcion, int cantidad, double precio)
 {
     listaProductos = listaProductos + descripcion + '\n';
     subtotal = subtotal + (cantidad * precio);
+    total = subtotal + (subtotal * 0.15 );
+
 }
 
 void imprimirFactura()
@@ -27,6 +32,10 @@ void imprimirFactura()
     cout << "Subtotal: " << subtotal;
     cout << endl;
     cout << endl;
+    cout << "Total: " << total;
+    cout << endl;
+    cout << endl;
+
     system("pause");
 
 }
